169c4914694943c812103b649be9d8e254fb8fa7
unknown <2998534583@qq.com>
2024-07-25 15:32:44 +0800

    完成大屏UI

--- ruoyi-ui/src/main.js
+++ ruoyi-ui/src/main.js
@@ -80,6 +80,27 @@ DictData.install()
  * Currently MockJs will be used in the production environment,
  * please remove it before going online! ! !
  */
+//数据大屏引入
+import dataV from '@jiaminghi/data-view';
+// 按需引入vue-awesome图标
+import Icon from 'vue-awesome/components/Icon';
+import 'vue-awesome/icons/chart-bar.js';
+import 'vue-awesome/icons/chart-area.js';
+import 'vue-awesome/icons/chart-pie.js';
+import 'vue-awesome/icons/chart-line.js';
+import 'vue-awesome/icons/align-left.js';
+//引入echart
+//4.x 引用方式
+// import echarts from 'echarts'
+//5.x 引用方式为按需引用
+//希望使用5.x版本的话,需要在package.json中更新版本号,并切换引用方式
+import * as echarts from 'echarts'
+Vue.prototype.$echarts = echarts
+Vue.config.productionTip = false;
+// 全局注册
+Vue.use(dataV);
+
+
 
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium' // set element-ui default size
